9f4ecd5beaadd3dda4659a5e7ec7bbe546c27561
xingyuewu <qi_zhengning@163.com>
2017-09-29 12:38:42 +0800

    2

--- application/index/controller/Pay.php
+++ application/index/controller/Pay.php
@@ -58,8 +58,34 @@ class Pay extends Base{
 
 
 
+
             return [
                 'goods'=>$data,
                   'total'=>  $total];
+    }
+
+    public function order(){
+        //获取登录用户的id
+        $id=session('member')['member_id'];
+        //查询购物车表、商品表和图片管理表
+        $data=db('car')
+            ->alias('c')//别名
+            ->join('goods g','g.goods_id=c.goods_id','left')
+            ->field('c.goods_num,g.sell_price')
+            ->where(['c.member_id'=>$id])
+            ->select();
+        //计算总价
+        $total = '';
+        foreach ($data as $k=>$v){
+            $total += $v['sell_price']*$v['goods_num'];
+        }
+        //获取下单时间
+        $create_time=time();
+        //把需要存到order表的数据放到arr数组
+        $arr=[
+            'member_id'=>$id,
+            'total'=>$total,
+            'create_time'=>$create_time,
+        ];
     }
 }
